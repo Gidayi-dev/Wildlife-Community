acdfcc4d0be8de8c804b19d8fc53717068740305
Gidayi Millyannah <millyannahi@gmail.com>
2024-10-26 16:21:07 +0300

    header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/header";
+// check out menu icon
 
 function App() {
   return (
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,47 +1,39 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import "./header.css";
 
 function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
-  return (
-    <header className={`navbar ${isMenuOpen ? "menu-open" : ""}`}>
-      <div className="menu-icon" onClick={toggleMenu}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-
-      <nav className="navbar-center">
-        <a href="/" className="nav-link">
-          Home
-        </a>
-        <a href="/about" className="nav-link">
-          About Us
-        </a>
-        <a href="/explore" className="nav-link">
-          Explore
-        </a>
-        <a href="/conservation" className="nav-link">
-          Conservation
-        </a>
-        <a href="/contact" className="nav-link">
-          Contact Us
-        </a>
-        <div className="navbar-auth">
-          <button className="button">Login</button>
-          <button className="button">Sign Up</button>
-        </div>
-      </nav>
-
-      <div className="navbar-right">
-        <img src="src/assets/logo.png" alt="logo" />
-      </div>
-    </header>
-  );
+    return (
+        <header className='navbar'>
+            <div className="navbar-left">
+                <img src="src/assets/logo.png" alt="logo" />
+            </div>
+            <div className="navbar-right">
+                <button className='menu-toggle' onClick={toggleMenu}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </button>
+                <div className={`menu-items ${isMenuOpen ? 'open' : ''}`}>
+                    <nav className='navbar-center'>
+                        <a href="/" className='nav-link'>Home</a>
+                        <a href="/about" className='nav-link'>About Us</a>
+                        <a href="/explore" className='nav-link'>Explore</a>
+                        <a href="/conservation" className='nav-link'>Conservation</a>
+                        <a href="/contact" className='nav-link'>Contact Us</a>
+                    </nav>
+                    <div className='navbar-buttons'>
+                        <button className='button' onClick={toggleMenu}>Login</button>
+                        <button className='button' onClick={toggleMenu}>Sign Up</button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
