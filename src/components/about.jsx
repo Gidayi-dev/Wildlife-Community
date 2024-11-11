8e423225d69f8af82137754490be4877ef2134b6
Gidayi Millyannah <millyannahi@gmail.com>
2024-11-11 18:09:58 +0300

    explore

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -17,30 +17,38 @@ function About() {
               bridge gaps, spark progress, and uplift communities worldwide. Our
               initial projects focused on developing local solutions with global
               potential, aiming to create practical resources that could benefit
-              individuals and organizations alike.</p><br />  <p> From the outset, we
-              prioritized understanding the unique challenges faced by different
-              communities, adapting our methods to suit diverse needs. This
-              adaptable, people-first approach led us to partner with grassroots
-              organizations, government bodies, and other key stakeholders. Each
-              collaboration expanded our capabilities, and we developed an array
-              of tools and strategies that continue to empower communities
-              across the globe. Over the years, our growth has been fueled by
-              our commitment to lasting impact. Today, we stand as a leader in
-              our field, with a track record of successful initiatives that
-              blend technology, innovation, and human insight to address some of
-              the world’s most pressing issues.</p><br />  <p> Our journey is not just a
-              timeline of milestones but a testament to the power of collective
-              effort. As we look to the future, we remain devoted to advancing
-              solutions that are sustainable, inclusive, and transformative.
-              With each new project, we reaffirm our commitment to empowering
-              individuals, driving progress, and inspiring a world where
-              meaningful change is within reach for all.
+              individuals and organizations alike.
+            </p>
+            <br />{" "}
+            <p>
+              {" "}
+              From the outset, we prioritized understanding the unique
+              challenges faced by different communities, adapting our methods to
+              suit diverse needs. This adaptable, people-first approach led us
+              to partner with grassroots organizations, government bodies, and
+              other key stakeholders. Each collaboration expanded our
+              capabilities, and we developed an array of tools and strategies
+              that continue to empower communities across the globe. Over the
+              years, our growth has been fueled by our commitment to lasting
+              impact. Today, we stand as a leader in our field, with a track
+              record of successful initiatives that blend technology,
+              innovation, and human insight to address some of the world’s most
+              pressing issues.
+            </p>
+            <br />{" "}
+            <p>
+              {" "}
+              Our journey is not just a timeline of milestones but a testament
+              to the power of collective effort. As we look to the future, we
+              remain devoted to advancing solutions that are sustainable,
+              inclusive, and transformative. With each new project, we reaffirm
+              our commitment to empowering individuals, driving progress, and
+              inspiring a world where meaningful change is within reach for all.
             </p>
           </div>
           <div className="video">
             <video src={videoSrc} className="video" controls></video>
           </div>
-
         </div>
       </div>
 
@@ -105,7 +113,7 @@ function About() {
           include:
         </p>
         <ul>
-        <br />
+          <br />
           <li>
             <b>2012:</b> Launched our first initiative, impacting over 10,000
             lives through grassroots programs focused on education and community
@@ -116,20 +124,20 @@ function About() {
             <b>2015:</b>Expanded our reach internationally, forming partnerships
             with organizations and stakeholders across different sectors.
           </li>
-          <br /> 
+          <br />
           <li>
             <b>2018:</b>Introduced our innovation lab, where our experts and
             partners collaborate to develop sustainable solutions that address
             pressing global challenges.
           </li>
-          <br /> 
+          <br />
           <li>
             <b>2020-Present:</b>We have continued to grow, launching new
             initiatives that span environmental sustainability, social justice,
             and technology access, consistently striving to make a larger, more
             meaningful impact.
           </li>
-          <br /> 
+          <br />
         </ul>
         <p>
           Our journey is fueled by the belief that small actions, combined with
--- src/components/explore.jsx
+++ src/components/explore.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './exploreCarousel.css';
+import React, { useState, useEffect } from "react";
+import "./exploreCarousel.css";
 import Rhino from "/src/assets/rhino.jpg";
 import Tsavo from "/src/assets/tiger.jpg";
 import Watamu from "/src/assets/watamu.jpg";
@@ -9,13 +9,30 @@ import SA from "/src/assets/sa.jpg";
 import USA from "/src/assets/usa.jpg";
 
 const slides = [
-  { image: Rhino, description: 'Masai Mara: Known for the Great Migration.' },
-  { image: Tsavo, description: 'Tsavo National Park: Famous for red elephants.' },
-  { image: Watamu, description: 'Watamu Marine Park: Coral reefs and sea turtles.' },
-  { image: Amazon, description: 'Amazon Rainforest: The lungs of the Earth.' },
-  { image: SA, description: 'Kruger National Park (South Africa): This is one of the oldest and most established parks in Africa, spanning diverse ecosystems.' },
-  { image: Gala, description: 'Galápagos Islands: Unique species and conservation.' },
-  { image: USA, description: 'Yellowstone National Park (USA): Recognized as the first national park in the world, Yellowstone is famous for geothermal features and diverse species, like bison and wolves.' },
+  { image: Rhino, description: "Masai Mara: Known for the Great Migration." },
+  {
+    image: Tsavo,
+    description: "Tsavo National Park: Famous for red elephants.",
+  },
+  {
+    image: Watamu,
+    description: "Watamu Marine Park: Coral reefs and sea turtles.",
+  },
+  { image: Amazon, description: "Amazon Rainforest: The lungs of the Earth." },
+  {
+    image: SA,
+    description:
+      "Kruger National Park (South Africa): This is one of the oldest and most established parks in Africa, spanning diverse ecosystems.",
+  },
+  {
+    image: Gala,
+    description: "Galápagos Islands: Unique species and conservation.",
+  },
+  {
+    image: USA,
+    description:
+      "Yellowstone National Park (USA): Recognized as the first national park in the world, Yellowstone is famous for geothermal features and diverse species, like bison and wolves.",
+  },
 ];
 
 const Explore = () => {
@@ -29,32 +46,44 @@ const Explore = () => {
   }, []);
 
   return (
-    <div className='explore-description'>
-        <h1>
-            Explore Beautiful sceneries accross the world
-        </h1>
-        <div className='par'><p>Embark on a virtual journey through breathtaking game parks, vibrant marine sanctuaries, and iconic global biodiversity hotspots. Discover Kenya’s stunning game reserves like Masai Mara and Tsavo, where majestic wildlife like elephants, lions, and the Great Migration unfold. Dive into the vibrant underwater worlds of Watamu and the Great Barrier Reef, brimming with marine life and colorful corals. Finally, expand your adventure to global treasures like the Amazon Rainforest and Serengeti, where conservation efforts protect our planet's rich natural diversity. This interactive exploration will inspire you to appreciate and support the preservation of these vital ecosystems.</p></div>
-        
-    <div className="carousel">
-      {slides.map((slide, index) => (
-        <div
-          key={index}
-          className={`slide ${index === currentIndex ? 'active' : ''}`}
-        >
-          <img src={slide.image} alt="wildlife" className="carousel-image" />
-          <p className="carousel-description">{slide.description}</p>
-        </div>
-      ))}
-      <div className="carousel-dots">
-        {slides.map((_, index) => (
-          <span
-            key={index}
-            className={`dot ${index === currentIndex ? 'active' : ''}`}
-            onClick={() => setCurrentIndex(index)}
-          />
-        ))}
+    <div className="explore-description">
+      <h1>Explore Beautiful sceneries accross the world</h1>
+      <div className="par">
+        <p>
+          Embark on a virtual journey through breathtaking game parks, vibrant
+          marine sanctuaries, and iconic global biodiversity hotspots. Discover
+          Kenya’s stunning game reserves like Masai Mara and Tsavo, where
+          majestic wildlife like elephants, lions, and the Great Migration
+          unfold. Dive into the vibrant underwater worlds of Watamu and the
+          Great Barrier Reef, brimming with marine life and colorful corals.
+          Finally, expand your adventure to global treasures like the Amazon
+          Rainforest and Serengeti, where conservation efforts protect our
+          planet's rich natural diversity. This interactive exploration will
+          inspire you to appreciate and support the preservation of these vital
+          ecosystems.
+        </p>
       </div>
-    </div>
+
+      <div className="carousel">
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className={`slide ${index === currentIndex ? "active" : ""}`}
+          >
+            <img src={slide.image} alt="wildlife" className="carousel-image" />
+            <p className="carousel-description">{slide.description}</p>
+          </div>
+        ))}
+        <div className="carousel-dots">
+          {slides.map((_, index) => (
+            <span
+              key={index}
+              className={`dot ${index === currentIndex ? "active" : ""}`}
+              onClick={() => setCurrentIndex(index)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
